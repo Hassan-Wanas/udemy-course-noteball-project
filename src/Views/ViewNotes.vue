65312e5d917296fbf1ce8c1bdf268b7a3c0ab16c
حسان ونس <Hassan0Wanas@Gmail.com>
2024-05-25 09:38:44 +0300

    fix textarea autofocus

--- src/Views/ViewNotes.vue
+++ src/Views/ViewNotes.vue
@@ -1,28 +1,13 @@
 <template>
   <div class="notes">
 
-    <AddEditNote>
+    <AddEditNote v-model="newNote" ref="addEditNoteRef">
       <template #buttons>
         <button @click="addNote" :disabled="!newNote" class="button is-link has-background-success">Add New
           Note</button>
       </template>
     </AddEditNote>
-    <!--
-    <div class="container has-background-success-dark p-4 mb-5 box">
-      <div class="field">
-        <div class="control">
-          <textarea v-model="newNote" class="textarea" placeholder="Add a new note" ref="newNoteRef" />
-        </div>
-      </div>
 
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button @click="addNote" :disabled="!newNote" class="button is-link has-background-success">Add New
-            Note</button>
-        </div>
-      </div>
-    </div>
--->
     <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
   </div>
 </template>
@@ -43,12 +28,12 @@ const storeNotes = useStoreNotes()
 // notes
 
 const newNote = ref('')
-const newNoteRef = ref(null)
+const addEditNoteRef = ref(null)
 
 const addNote = () => {
   storeNotes.addNote(newNote.value)
   newNote.value = ''
-  newNoteRef.value.focus()
+  addEditNoteRef.value.focusTextArea()
 }
 
 </script>
--- src/components/Notes/AddEditNotes.vue
+++ src/components/Notes/AddEditNotes.vue
@@ -2,14 +2,40 @@
   <div class="container has-background-success-dark p-4 mb-5 box">
     <div class="field">
       <div class="control">
-        <textarea class="textarea" placeholder="Add a new note" ref="newNoteRef" />
+        <textarea :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" class="textarea"
+          placeholder="Add a new note" ref="textareaRef" />
       </div>
     </div>
 
     <div class="field is-grouped is-grouped-right">
       <div class="control">
-        <slot name="buttons"/>
+        <slot name="buttons" />
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+// props
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+// focus TextArea
+
+const textareaRef = ref(null)
+
+const focusTextArea = () => {
+  textareaRef.value.focus()
+}
+
+defineExpose({
+  focusTextArea
+})
+</script>
