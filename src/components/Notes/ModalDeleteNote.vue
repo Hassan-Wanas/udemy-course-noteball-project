4218808a209b9f05e16222c4eae4bad0af367179
حسان ونس <Hassan0Wanas@Gmail.com>
2024-05-30 22:11:27 +0300

    make delete modal design and make it hidden by default

--- src/components/Notes/ModalDeleteNote.vue
+++ src/components/Notes/ModalDeleteNote.vue
@@ -1,20 +1,55 @@
 <template>
-  <div class="modal is-active">
+  <div ref="deleteModalRef" class="modal is-active">
     <div class="modal-background"></div>
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Delete Note ?</p>
-        <button class="delete" aria-label="close"></button>
+        <button @click="closeModal" class="delete" aria-label="close"></button>
       </header>
       <section class="modal-card-body">
         Are you sure you want to delete this note ?
       </section>
       <footer class="modal-card-foot is-justify-content-flex-end">
         <div class="buttons">
-          <button class="button">Cancel</button>
+          <button @click="closeModal" class="button">Cancel</button>
           <button class="button is-danger">Delete</button>
         </div>
       </footer>
     </div>
   </div>
 </template>
+
+<script setup>
+
+// imports
+
+import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
+
+// props
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false
+  }
+})
+
+// emits
+
+const emits = defineEmits(['update:modelValue'])
+
+// close modal
+
+const closeModal = () => {
+  emits('update:modelValue', false)
+}
+
+// click outside to close
+
+const deleteModalRef = ref(null)
+
+onClickOutside(deleteModalRef, () => {
+  emits('update:modelValue', false)
+})
+</script>
