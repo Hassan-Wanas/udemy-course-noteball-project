28f9114c6af1d51806ec53c813481fea015eac10
حسان ونس <Hassan0Wanas@Gmail.com>
2024-06-04 15:40:55 +0300

    initializing auth page

---
+++ src/stores/storeAuth.js
@@ -0,0 +1,14 @@
+// stores/storeNote.js
+import { defineStore } from 'pinia'
+import { auth } from '@/js/Firebase/storeAuth.js';
+
+export const useStoreAuth = defineStore('storeAuth', {
+  state: () => {
+    return { 
+      
+    }
+  },
+  actions: {
+    
+  },
+})
